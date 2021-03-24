0e2e6aa6d0df81e0c7349ff6bb1459715db82abe
Tim <37161147+Dovvah@users.noreply.github.com>
2021-03-24 00:37:44 -0500

    Begin implementing obj1 writer.

--- Program.cs
+++ Program.cs
@@ -13,31 +13,36 @@ namespace WoWLadder
         static void Main(string[] args)
         {
             string FolderPath = args[0];
-            DirectoryInfo AdtFiles = new DirectoryInfo(FolderPath);
+            string[] AdtFiles = Directory.GetFiles(FolderPath);
             if (args.Length == 0)
                 return;
             else
             {
 
-                foreach (var file in AdtFiles.GetFiles("*.adt"))
-                    if (file.Name.Contains("_tex"))
+                foreach (var file in AdtFiles)
+                    if (file.Contains("_tex"))
                     {
-
-
-                        Console.WriteLine("Found _tex " + file.Name + Environment.NewLine);
-                    
-                       
+                        Console.WriteLine("tex file found, ignoring" + file + Environment.NewLine);
                     }
                     else
-                    if (file.Name.Contains("_obj"))
+                    if (file.Contains("_obj0"))
                     {
-                        Console.WriteLine(file.Name + "obj file found" +  Environment.NewLine);
-
-
+                        Console.WriteLine("obj0 file found, ignoring" + file + Environment.NewLine);
                     }
-                else
+                    else
+                    if (file.Contains("_obj1"))
                     {
-                        Console.WriteLine("Normal .adt file found" + file.Name + Environment.NewLine);
+                        Console.WriteLine("obj0 file found, processing." + file + Environment.NewLine);
+                        using (Stream obj1stream = File.Open(file, FileMode.Open, FileAccess.ReadWrite)) 
+                            using (BinaryReader obj1reader = new BinaryReader(obj1stream))
+                            using (BinaryWriter obj1writer = new BinaryWriter(obj1stream))
+                        {
+
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Normal .adt file found" + file + Environment.NewLine);
                     }
                 Thread.Sleep(50000);
             }
